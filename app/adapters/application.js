c121dbb44cd9f33b74dd1c385dcc9612fdbfdf4d
adam moore <adamaoc@gmail.com>
2015-10-20 06:27:24 -0500

    updating stats widget to be number formatted

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.RESTAdapter.extend({
   // namespace: '/',
-  host: 'http://api.txgarage.com'
-  // host: 'http://localhost:8888'
+  // host: 'http://api.txgarage.com'
+  host: 'http://localhost:8888'
 });
