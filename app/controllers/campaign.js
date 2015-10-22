e2299f5c232f3da9ba502b4566e78190a14b18b2
adam moore <adamaoc@gmail.com>
2015-10-22 06:27:47 -0500

    updating styles and added sass

--- app/controllers/campaign.js
+++ app/controllers/campaign.js
@@ -4,4 +4,8 @@ export default Ember.Controller.extend({
   campaign: Ember.computed('model', function() {
     return this.get('model.firstObject');
   }),
+  clicks: Ember.computed('campaign', function() {
+    const campaign = this.get('campaign.slug');
+    return this.store.find('click', {campaign: campaign});
+  })
 });
